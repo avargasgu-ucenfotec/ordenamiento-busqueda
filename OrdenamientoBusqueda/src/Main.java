492d35ee97e1c72510d33fac0f317934d43c05f6
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-10-27 19:38:59 -0600

    Agrega menu

--- OrdenamientoBusqueda/src/Main.java
+++ OrdenamientoBusqueda/src/Main.java
@@ -1,22 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scannerInt = new Scanner(System.in);
-        Scanner scannerString = new Scanner(System.in);
-        System.out.print("Digite la cantidad de palabras que desea ingresar: ");
-
-        String[] palabras = new String[scannerInt.nextInt()];
-
-        for (int i = 0; i < palabras.length; i++) {
-            System.out.print("Digite la siguiente palabra: ");
-            String palabra = scannerString.nextLine();
-            palabras[i] = palabra;
-        }
-
-        for (String palabra : palabras) {
-            System.out.print(palabra + " ");
-        }
+        Menu menu = new Menu();
+        menu.correrMenu();
     }
 }
---
+++ OrdenamientoBusqueda/src/Menu.java
@@ -0,0 +1,89 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Menu {
+
+    //Clase "Menu" que despliega un mensaje de bienvenida, solicita información al usuario y muetra un menú en consola
+    //que permite al usuario elegir entre cinco algoritmos de ordenamiento.
+
+    //Atributos
+    private boolean estaCorriendo;  //Bandera que indica si el menú está corriendo.
+
+    //Métodos
+    //Constructor
+    public Menu() {
+        estaCorriendo = true;
+    }
+
+    //Funcionales
+    public void correrMenu() {
+        int cantidad = capturarCantidad();
+        String[] arregloPalabras = generarArreglo(cantidad);
+        while (estaCorriendo) {
+            desplegarMenu();
+            obtenerOpcion(arregloPalabras);
+        }
+    }
+
+    public int capturarCantidad() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEstrategia de aprendizaje - Algoritmos de ordenamiento y búsqueda");
+        System.out.print("Digite la cantidad de palabras que desea ingresar: ");
+        return scanner.nextInt();
+    }
+
+    public String[] generarArreglo(int cantidadElementos) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arreglo = new String[cantidadElementos];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("Digite una palabra: ");
+            String palabra = scanner.nextLine();
+            arreglo[i] = palabra;
+        }
+        return arreglo;
+    }
+
+    public void desplegarMenu() {
+        System.out.println("\nMenú de algoritmos de ordenamiento");
+        System.out.println("[1] Selección");
+        System.out.println("[2] Inserción");
+        System.out.println("[3] Burbuja");
+        System.out.println("[4] Mezcla");
+        System.out.println("[5] Rápido");
+        System.out.println("[6] Imprimir arreglo de palabras");
+        System.out.println("[7] Salir");
+        System.out.print("Seleccione una opción del menú: ");
+    }
+
+    public void obtenerOpcion(String[] arreglo) {
+        Scanner scanner = new Scanner(System.in);
+        String seleccion = scanner.nextLine();
+        switch (seleccion) {
+            case "1":
+                System.out.println("\nAlgoritmo de selección\n");
+                break;
+            case "2":
+                System.out.println("\nAlgoritmo de inserción\n");
+                break;
+            case "3":
+                System.out.println("\nAlgoritmo de burbuja\n");
+                break;
+            case "4":
+                System.out.println("\nAlgoritmo de mezcla\n");
+                break;
+            case "5":
+                System.out.println("\nAlgoritmo rápido\n");
+                break;
+            case "6":
+                System.out.print("\nArreglo: ");
+                System.out.println(Arrays.toString(arreglo));
+                break;
+            case "7":
+                System.out.println("\nCerrando el programa...");
+                estaCorriendo = false;
+                break;
+            default:
+                System.out.println("\nOpción no válida, por favor intente de nuevo.");
+        }
+    }
+}
